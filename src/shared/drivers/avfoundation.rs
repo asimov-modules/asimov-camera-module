15d39eb0a929652d79a77e5000611cb8974bd7e9
Arto Bendiken <arto@asimov.systems>
2025-11-27 23:23:19 -0800

    Create stubs for backend drivers.

--- src/shared/drivers/avfoundation.rs
+++ src/shared/drivers/avfoundation.rs
@@ -1,6 +1,1 @@
 // This is free and unencumbered software released into the public domain.
-
-#![forbid(unsafe_code)]
-
-pub mod core;
-pub mod shared;
---
+++ src/shared/mod.rs
@@ -0,0 +1,18 @@
+// This is free and unencumbered software released into the public domain.
+
+pub mod drivers {
+    #[cfg(feature = "ffmpeg")]
+    pub mod ffmpeg;
+
+    #[cfg(all(not(feature = "ffmpeg"), any(target_os = "macos", target_os = "ios")))]
+    pub mod avfoundation;
+
+    #[cfg(all(not(feature = "ffmpeg"), target_os = "android"))]
+    pub mod camera2;
+
+    #[cfg(all(not(feature = "ffmpeg"), target_os = "windows"))]
+    pub mod dshow;
+
+    #[cfg(all(not(feature = "ffmpeg"), target_os = "linux"))]
+    pub mod v4l2;
+}

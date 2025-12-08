d4ea6e48246fc09318fc695312144860c9f1df49
Oleksnadr Yakovenko <sanches2026@gmail.com>
2025-12-08 12:55:26 -0500

    Initial Commit

---
+++ RustCameraTest/RustCameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustCameraTestApp.swift
+//  RustCameraTest
+//
+//  Created by Alex on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RustCameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

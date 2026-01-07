ed210c872b96cc83c03e5c8f467aa5f7c9ab3556
Oleksnadr Yakovenko <sanches2026@gmail.com>
2026-01-07 11:43:53 -0500

    update build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,8 @@
 fn main() {
-    // Only for Android targets
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("android") {
-        // Camera2 NDK
         println!("cargo:rustc-link-lib=camera2ndk");
-        // Media NDK (AImageReader / AImage)
         println!("cargo:rustc-link-lib=mediandk");
 
-        // Обычно не нужно, но пусть будет явным:
         println!("cargo:rustc-link-lib=android");
         println!("cargo:rustc-link-lib=log");
     }
